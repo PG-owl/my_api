29cfd87cb90772c2c9d47e75ea9ed8665762a539
T.T <your.email@example.com>
2022-04-12 00:29:55 +0900

    implementation delete function

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,8 +3,28 @@ module Api
     class PostsController < ApplicationController
   
       def index
-        posts = Post.all
-        render :json => posts
+        @posts = Post.all
+        render json: @posts
+      end
+
+      def create
+        @posts = Post.create(task: params[:task])
+        rebder json: @posts
+      end
+
+      def update
+        @posts = Poat.find(params[:id])
+        @posts.update_attributes(isCompleted: params[:isCompleted])
+        render json: @posts
+      end
+
+      def destroy
+        @posts = Post.find(params[:id])
+        if @posts.destroy
+          head :no_content, status: :ok
+        else
+          render json: @posts.errors, status: :unprocessable_entity
+        end
       end
 
     end
